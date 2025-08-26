c6653f0a77c56a4e185c0542f9550518fca49d9c
Felipao22 <felipeaviani@gmail.com>
2025-08-26 20:22:25 -0300

    feat: agrego llamada para obtener archivos según categoría y su respectivo tipado.

--- services/filesApi.ts
+++ services/filesApi.ts
@@ -7,6 +7,11 @@ export interface Kind {
   categoryId: number;
 }
 
+export interface FileResponse {
+  message: string;
+  data: File[];
+}
+
 export const filesApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getFiles: builder.query<File[], void>({
@@ -17,8 +22,17 @@ export const filesApi = apiSlice.injectEndpoints({
       query: () => "/kind",
       providesTags: ["Kinds"],
     }),
+    getFilesByKindId: builder.query<File[], string>({
+      query: (kindId) => `/file/kind/${kindId}`,
+      providesTags: ["File"],
+      transformResponse: (response: FileResponse) => response.data,
+    }),
   }),
   overrideExisting: true,
 });
 
-export const { useGetFilesQuery, useGetKindsFilesQuery } = filesApi;
+export const {
+  useGetFilesQuery,
+  useGetKindsFilesQuery,
+  useLazyGetFilesByKindIdQuery,
+} = filesApi;
