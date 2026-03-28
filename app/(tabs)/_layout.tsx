c00093153bdea5b532049bb166b485403f67feff
Felipao22 <felipeaviani@gmail.com>
2026-03-28 17:08:04 -0300

    feat: Actualizar el diseño y la personalización de la aplicación para que sean compatibles con la retroalimentación háptica

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,90 +1,47 @@
 import { Tabs } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import {
-  House,
-  ClipboardCheck,
-  TriangleAlert as AlertTriangle,
-  GraduationCap,
-  User,
-  File,
-} from "lucide-react-native";
-import { Platform } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import React from "react";
+
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { File, House } from "lucide-react-native";
 
 export default function TabLayout() {
-  const insets = useSafeAreaInsets();
-
-  const MIN_TABBAR_HEIGHT_ANDROID = 56;
-  const MIN_TABBAR_HEIGHT_IOS = 50;
-
-  const bottomInset = insets.bottom || 0;
-
-  // Calculamos la altura total de la tabBar sumando la altura mínima y el safe area inferior.
-  const tabBarHeight =
-    Platform.OS === "android"
-      ? MIN_TABBAR_HEIGHT_ANDROID + bottomInset
-      : MIN_TABBAR_HEIGHT_IOS + bottomInset;
+  const colorScheme = useColorScheme();
 
   return (
-    <>
-      <StatusBar style="dark" />
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "#059669",
-          tabBarInactiveTintColor: "#64748b",
-          tabBarStyle: {
-            height: tabBarHeight,
-            paddingBottom: bottomInset,
-            paddingTop: 8,
-            backgroundColor: "#fff",
-            elevation: 8,
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: -2 },
-            shadowOpacity: 0.1,
-            shadowRadius: 3,
-            borderTopWidth: 1,
-            borderTopColor: "#ccc",
-          },
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "600",
-            marginTop: 2,
-            marginBottom: 4,
-          },
-          tabBarIconStyle: {
-            marginBottom: -4,
-          },
-          tabBarShowLabel: true,
-          tabBarAllowFontScaling: false,
-          tabBarHideOnKeyboard: true,
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="Dashboard"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ size, color }: { size: number; color: string }) => (
+            <House size={size} color={color} />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="Dashboard"
-          options={{
-            title: "Dashboard",
-            tabBarIcon: ({ size, color }: { size: number; color: string }) => (
-              <House size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="FilesScreen"
-          options={{
-            title: "Archivos",
-            tabBarIcon: ({ size, color }: { size: number; color: string }) => (
-              <File size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="ConstanciaVisitaScreen"
-          options={{
-            href: null,
-          }}
-        />
-      </Tabs>
-    </>
+      />
+      <Tabs.Screen
+        name="FilesScreen"
+        options={{
+          title: "Archivos",
+          tabBarIcon: ({ size, color }: { size: number; color: string }) => (
+            <File size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ConstanciaVisitaScreen"
+        options={{
+          href: null,
+        }}
+      />
+    </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,22 @@ import { Stack } from "expo-router";
 import { store } from "../store/index";
 import { Provider } from "react-redux";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <SafeAreaProvider>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Provider store={store}>
-        <StatusBar style="dark" />
         <Stack screenOptions={{ headerShown: false }} />
       </Provider>
-    </SafeAreaProvider>
+      <StatusBar style="dark" />
+    </ThemeProvider>
   );
 }
